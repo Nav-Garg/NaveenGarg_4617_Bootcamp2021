4dd45c024ab646ef36d0df65a14ba3ab8fa48677
Naveen <gargnaveen96@gmail.com>
2021-03-05 21:54:08 +0530

    /home/ttn/Documents/Spring_boot_assignments/RestFul Web Service Part 1/restful-web-service-exercise

---
+++ restful-web-service-exercise/src/main/java/com/ttn/spring/springbootexercise/employee/Employee.java
@@ -0,0 +1,62 @@
+package com.ttn.spring.springbootexercise.employee;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Employee {
+    private Integer id;
+    @NotNull
+    @Size(min=2, max=30, message = "Name Should have atleast 2 letter")
+    private String name;
+
+    @NotNull
+    @Min(value = 20, message = "minimum age required 20 year")
+    private Integer age;
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
