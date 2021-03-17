4a536431c2f31b40e065fc484146af35d293b355
Naveen <gargnaveen96@gmail.com>
2021-03-17 13:01:42 +0530

    /home/ttn/Documents/Spring_boot_assignments/security/spring-assignment

---
+++ spring-assignment/src/test/java/com/springsecurity/springassignment/SpringAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
