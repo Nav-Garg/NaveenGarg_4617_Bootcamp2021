1a340cf0ab7ebcc2eca94195f03d81e97f327182
Naveen Garg <46532058+Nav-Garg@users.noreply.github.com>
2021-03-17 13:04:34 +0530

    Delete spring-assignment directory

--- spring-assignment/src/main/java/com/springsecurity/springassignment/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springsecurity.springassignment;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByUsername(String username);
-}
